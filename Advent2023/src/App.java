a8d63c1f7b1cb39538e9e756e7a78d31a2f9b01a
hw <hopefully.purple@gmail.com>
2023-12-02 14:19:24 -0800

    call Trebuchet from App

--- Advent2023/src/App.java
+++ Advent2023/src/App.java
@@ -1,5 +1,9 @@
+import Day1.Trebuchet;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Trebuchet.main("input from App.main");
     }
 }
--- Advent2023/src/Day1/Trebuchet.java
+++ Advent2023/src/Day1/Trebuchet.java
@@ -4,5 +4,7 @@ package Day1;
  * Day 1: Trebuchet?!
  */
 public class Trebuchet {
-    
+    public static void main(String x) {
+        System.out.println(x);
+    }
 }
